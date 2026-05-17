fab9f183172653d252a5adebe7596e76209d7ca2
OmniExplorer <omni.meulabs@gmail.com>
2026-05-17 17:56:21 +0530

    Mobile Optimisation

--- components/home-page.js
+++ components/home-page.js
@@ -164,11 +164,11 @@ export default function HomePage({
                     className={
                       family.id === "foundations"
                         ? isNzBranch
-                          ? "pb-3 pl-4 pr-0 -mr-2 sm:pl-5 sm:-mr-3"
+                          ? "pb-3 pl-3 pr-3 sm:pr-0 sm:pl-5 sm:-mr-3"
                           : "pb-3 pl-3 pr-0 sm:pl-4 sm:pr-0"
                         : isNzBranch
-                          ? "pb-3 pl-7 pr-0 -mr-6 sm:pl-8 sm:-mr-7"
-                          : "pb-3 pl-5 pr-0 -mr-3 sm:pl-6 sm:-mr-4"
+                          ? "pb-3 pl-3 pr-3 sm:pr-0 sm:pl-8 sm:-mr-7"
+                          : "pb-3 pl-3 pr-2 sm:pr-0 sm:pl-6 sm:-mr-4"
                     }
                   >
                     <div className="grid gap-3">
